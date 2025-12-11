94d792d0629a675fdaf13a26e74aa30a02d61918
nguien <nguyen10t2lhp@gmail.com>
2025-12-11 20:40:29 +0700

    feat: add authentication and authorization plugins for user routes

---
+++ src/plugins/authorizationPlugins.ts
@@ -0,0 +1,17 @@
+import Elysia from "elysia";
+import { ContextStore } from "../types/context";
+
+
+export const authorizationPlugins = (role: number) => (app: Elysia) => app
+    .onBeforeHandle(async (ctx) => {
+        const user = (ctx.store as ContextStore).user || undefined;
+        if (!user) {
+            ctx.set.status = 401;
+            return { error: 'Unauthorized' };
+        }
+
+        if (user.role < role) {
+            ctx.set.status = 403;
+            return { error: 'Forbidden' };
+        }
+    });
